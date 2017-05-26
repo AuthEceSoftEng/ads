3fd24060a9fadbcf57adfe8724b280745f437bc8
Elena Nisioti <elennisi@auth.gr>
2017-05-26 22:55:20 +0300

    Refactored package preprocessor
    Proposed future improvements:
    1. More transformations for FeatureEngineer (e.g. aggregation)

--- preprocessor/DataCompressor.R
+++ preprocessor/DataCompressor.R
@@ -1,99 +1,169 @@
-##' Α class responsible for performing PCA and MDA analysis on a dataset.
-##'
-##' @import methods caret
-##' @export
+#' Α class responsible for performing PCA and MDA analysis on a dataset.
+#' 
+#' @slot num_pca_attributes_ number of pca attributes
+#' @slot num_mda_attributes_ number of mda attributes
+#' @slot info_ list of information about DataCompressor
+
+#' @import methods
+#' @import caret
+#' @import FactoMineR
+#' 
+#' @export
 DataCompressor <- setRefClass(Class = "DataCompressor",
                               fields = list(
                                 info_ = "list",
                                 num_pca_attributes_ = "numeric",
                                 num_mda_attributes_ = "numeric"
-                              ),
-                              methods = list(
-                                performPCA = function(dataset, variance = 0.95, number_of_attributes = NULL, ...) {
-                                  'Finds PCA features with desired variance. Takes into account only numeric features.'
-                                  library(caret)
-                                  # remove categorical attributes
-                                  variables         <-  names(dataset[sapply(dataset,class) == "factor" | (sapply(dataset,function(x) class(x)[1])  == "ordered")])
-                                  num_dataset       <-  as.data.frame(dataset[, !(names(dataset) %in% variables)])
-                                  # remove Class
-                                  num_dataset$Class <- NULL
-                                  # apply PCA
-                                  if(ncol(num_dataset) != 0) {
-                                    pre.total_data      <- num_dataset
-                                    PCA                 <- prcomp(pre.total_data)
-                                    if(num_pca_attributes_ == 0) {
-                                      keep                <- (which(cumsum((PCA$sdev)^2) / sum(PCA$sdev^2)>=variance)[1])
-                                      num_pca_attributes_ <<- keep
-                                      trans_dataset       <- as.data.frame(PCA$x[, 1:keep])
-                                      colnames(trans_dataset) <- paste("PC", seq(1, keep), sep = "")
-                                    } else {
-                                      keep                <- num_pca_attributes_
-                                      if(ncol(PCA$x) < keep) {
-                                        
-                                        diff <- keep- ncol(PCA$x)
-                                        empty_data_frame <- as.data.frame(matrix(nrow = nrow(PCA$x), ncol = diff))
-                                        empty_data_frame[is.na(empty_data_frame)] <- 0
-                                        colnames(empty_data_frame) <- paste("PC", seq((ncol(PCA$x)+1) , keep), sep = "")
-                                        keep <- ncol(PCA$x)
-                                        trans_dataset       <- as.data.frame(PCA$x[, 1:keep])
-                                        colnames(trans_dataset) <- paste("PC", seq(1,ncol(PCA$x)), sep = "")
-                                        trans_dataset       <- cbind(trans_dataset, empty_data_frame)
-                                      } else {
-                                        trans_dataset       <- as.data.frame(PCA$x[, 1:keep])
-                                        colnames(trans_dataset) <- paste("PC", seq(1,keep), sep = "")
-                                      } 
-                                    }
-                                    trans_dataset$Class <- dataset$Class
-                                    # update info_ with info about PCA
-                                    pca_info  <- list(pertained_variance = variance, number_of_features = keep)
-                                    info_$PCA <<- pca_info
-                                  }
-                                  else {
-                                    trans_dataset    <- dataset
-                                  }
-                                  trans_dataset$Class <- dataset$Class
-                                  return((trans_dataset))
-                                },
-                                performMDA = function(dataset,variance = 0.95, number_of_attributes = NULL, ...) {
-                                  # prepare for MDA
-                                  variables           <-  names(dataset[sapply(dataset,class) == "factor" | (sapply(dataset,function(x) class(x)[1])  == "ordered")])
-                                  cat_dataset         <-  dataset[, (names(dataset) %in% variables)]
-                                  transformed_dataset <- cat_dataset
-                                  # cat_dataset$Class   <- NULL
-                                  # # apply MDA
-                                  # mca                 <- FactoMineR::MCA(cat_dataset, ncp = ncol(cat_dataset), graph=FALSE)
-                                  # if( num_mda_attributes_ == 0) {
-                                  #   st_dev              <- apply(mca$ind$coord, 2, sd)
-                                  #   keep                <- (which(cumsum((st_dev)^2) / sum(st_dev^2)>=variance)[1])
-                                  #   cat("first time")
-                                  #   num_mda_attributes_ <<- keep
-                                  # } else {
-                                  #   keep                <- num_mda_attributes_
-                                  # }
-                                  # trans_dataset       <- data.frame(mca$ind$coord)[, 1:keep]
-                                  # trans_dataset$Class <- dataset$Class
-                                  # # update info_ with info about PCA
-                                  # mda_info  <- list(pertained_variance = variance, number_of_features = keep)
-                                  # info_$MDA <<- mda_info
-                                  return(as.data.frame(transformed_dataset))
-                                  
-                                },
-                                getNumPCAAttributes  = function(...) {
-                                  return(num_pca_attributes_)
-                                },
-                                getNumMDAAttributes  = function(...) {
-                                  return(num_mda_attributes_)
-                                },
-                                getInfo = function(...) {
-                                  'Return information about data compression'
-                                  return(info_)
-                                },
-                                initialize = function(...) {
-                                  info_               <<- list()
-                                  num_pca_attributes_ <<- 0
-                                  num_mda_attributes_ <<- 0
-                                  callSuper(...)
-                                  .self
-                                }
+                                )
                               )
-)
+DataCompressor$methods(
+  #' Perform PCA analysis
+  #' 
+  #' This function supports two modes. By performing PCA analysis during training this function maintains the desired variance.
+  #' By applying PCA analysis during testing  \code{"num_pca_attributes_"} are pertained. 
+  #'
+  #' @name performPCA
+  #' @alias performPCA
+  #' 
+  #' @param dataset input dataset
+  #' @param variance percentage of pertained variance
+  #' @param number_of_attributes number of attributes pertained
+  #' 
+  #' @return dataset after application of PCA
+  performPCA = function(dataset, variance = 0.95, number_of_attributes = NULL, ...) {
+    'Finds PCA features with desired variance. Takes into account only numeric features.'
+    # ignore categorical attributes
+    variables         <-  names(dataset[sapply(dataset,class) == "factor" | (sapply(dataset,function(x) class(x)[1])  == "ordered")])
+    num_dataset       <-  as.data.frame(dataset[, !(names(dataset) %in% variables)])
+    # ignore Class
+    num_dataset$Class <- NULL
+    # apply PCA
+    if(ncol(num_dataset) != 0) {
+      pre.total_data      <- num_dataset
+      PCA                 <- prcomp(pre.total_data)
+      if(is.null(number_of_attributes)) { # first time performing PCA
+        keep                <- (which(cumsum((PCA$sdev)^2) / sum(PCA$sdev^2)>=variance)[1])
+        num_pca_attributes_ <<- keep
+        trans_dataset       <- as.data.frame(PCA$x[, 1:keep])
+        colnames(trans_dataset) <- paste("PC", seq(1, keep), sep = "")
+      } else { # applying PCA on test
+        keep                <- number_of_attributes
+        if(ncol(PCA$x) < keep) { # if features are correlated PCA may give fewer PC components
+          diff                                      <- keep - ncol(PCA$x)
+          empty_data_frame                          <- as.data.frame(matrix(nrow = nrow(PCA$x), ncol = diff))
+          empty_data_frame[is.na(empty_data_frame)] <- 0
+          colnames(empty_data_frame)                <- paste("PC", seq((ncol(PCA$x)+1) , keep), sep = "")
+          keep                                      <- ncol(PCA$x)
+          trans_dataset                             <- as.data.frame(PCA$x[, 1:keep])
+          colnames(trans_dataset)                   <- paste("PC", seq(1,ncol(PCA$x)), sep = "")
+          trans_dataset                             <- cbind(trans_dataset, empty_data_frame) # append empty data.frame
+        } else {
+          trans_dataset           <- as.data.frame(PCA$x[, 1:keep])
+          colnames(trans_dataset) <- paste("PC", seq(1,keep), sep = "")
+        } 
+      }
+      # update info_ with info about PCA
+      pca_info  <- list(pertained_variance = variance, number_of_features = keep)
+      info_$PCA <<- pca_info
+    }
+    else {
+      trans_dataset    <- dataset
+    }
+    trans_dataset$Class <- dataset$Class
+    return((trans_dataset))
+  },
+  #' Perform MDA analysis
+  #' 
+  #' This function supports two modes. By performing MDA analysis during training this function maintains the desired variance.
+  #' By applying MDA analysis during testing  \code{"num_pca_attributes_"} are pertained. 
+  #'
+  #' @name performMDA
+  #' @alias performMDA
+  #' 
+  #' @param dataset input dataset
+  #' @param variance percentage of pertained variance
+  #' @param number_of_attributes number of attributes pertained
+  #' 
+  #' @return dataset after application of MDA
+  performMDA = function(dataset, variance = 0.95, number_of_attributes = NULL,  ...) {
+    # ignore categorical attributes
+    variables         <-  names(dataset[sapply(dataset,class) == "factor" | (sapply(dataset,function(x) class(x)[1])  == "ordered")])
+    cat_dataset       <-  as.data.frame(dataset[, (names(dataset) %in% variables)])
+    # ignore Class
+    cat_dataset$Class <- NULL
+    # apply MDA
+    if(ncol(cat_dataset) != 0) {
+      mca                 <- MCA(cat_dataset, ncp = ncol(cat_dataset), graph=FALSE)
+      if(is.null(number_of_attributes)) { # first time performing MDA
+        st_dev              <- apply(mca$ind$coord, 2, sd)
+        keep                <- (which(cumsum((st_dev)^2) / sum(st_dev^2)>=variance)[1])
+        num_mda_attributes_ <<- keep
+        trans_dataset       <- data.frame(mca$ind$coord)[, 1:keep]
+        colnames(trans_dataset) <- paste("PC", seq(1, keep), sep = "")
+      } else { # applying MDA on test
+        keep                <- number_of_attributes
+        if(ncol(mca$x) < keep) { # if features are correlated MDA may give fewer MD components
+          diff                                      <- keep - ncol(mca$ind$coord)
+          empty_data_frame                          <- as.data.frame(matrix(nrow = nrow(mca$ind$coord), ncol = diff))
+          empty_data_frame[is.na(empty_data_frame)] <- 0
+          colnames(empty_data_frame)                <- paste("MD", seq((ncol(mca$ind$coord)+1) , keep), sep = "")
+          keep                                      <- ncol(mca$ind$coord)
+          trans_dataset                             <- as.data.frame(mca$ind$coord[, 1:keep])
+          colnames(trans_dataset)                   <- paste("MD", seq(1,ncol(mca$ind$coord)), sep = "")
+          trans_dataset                             <- cbind(trans_dataset, empty_data_frame) # append empty data.frame
+        } else {
+          trans_dataset           <- as.data.frame(mca$ind$coord[, 1:keep])
+          colnames(trans_dataset) <- paste("MD", seq(1,keep), sep = "")
+        } 
+      }
+      # update info_ with info about PCA
+      mda_info  <- list(pertained_variance = variance, number_of_features = keep)
+      info_$MDA <<- mda_info
+    }
+    else {
+      trans_dataset    <- dataset
+    }
+    trans_dataset$Class <- dataset$Class
+    return((trans_dataset))
+  },
+  #' Get number of PCA attributes
+  #' 
+  #' @name getNumPCAAttributes
+  #' @alias getNumPCAAttributes
+  #' 
+  #' 
+  #' @return number of PCA attributes
+  getNumPCAAttributes  = function(...) {
+    return(num_pca_attributes_)
+  },
+  #' Get number of MDA attributes
+  #' 
+  #' @name getNumMDAAttributes
+  #' @alias getNumMDAAttributes
+  #' 
+  #' 
+  #' @return number of MDA attributes
+  getNumMDAAttributes  = function(...) {
+    return(num_mda_attributes_)
+  },
+  #' Return information about DataCompressor
+  #' 
+  #' Information includes pertained variance and features after application of PCA and MDA>
+  #' 
+  #' @name getInfo
+  #' @alias getInfo
+  #' 
+  #' 
+  #' @return list of information
+  getInfo = function(...) {
+    'Return information about data compression'
+    return(info_)
+  },
+  initialize = function(...) {
+    info_               <<- list()
+    num_pca_attributes_ <<- 0
+    num_mda_attributes_ <<- 0
+    callSuper(...)
+    .self
+  }
+)                              
--- preprocessor/InapRemover.R
+++ preprocessor/InapRemover.R
@@ -1,77 +1,125 @@
-##' Α class responsible for removing inappropriate values from a dataset.
-##'
-##' As potential inappropriate values we consider unknown entries and outliers, which we treat accordingly.
-##' @import methods
-##' @export
+#' Α class responsible for removing inappropriate values from a dataset.
+#' 
+#' As potential inappropriate values we consider unknown entries, infinite values and outliers, which we treat accordingly.
+#' 
+#' @slot info_ list of information about InapRemover
+
+#' @import methods
+#' @import caret
+#' 
+#' @export
 InapRemover <- setRefClass(Class = "InapRemover",
                            fields = list(
                              info_ = "list"
-                             
-                           ),
-                           methods = list(
-                             removeUnknown = function(dataset, unknown_action = list( act = "replace", rep = 0), ...) {
-                               'Treats unknown values according to the action (a list indicating whether na entries should lead to deletion of rows or replacement with desired value)'
-                               dataset_treated <- dataset
-                               na_info         <- list( number = sum(is.na(dataset)), technique = unknown_action)
-                               info_$NAs       <<- na_info
-                               if(unknown_action$act == "delete") {
-                                 dataset_treated <- na.omit(dataset)
-                               }
-                               else if(unknown_action$act == "replace") {
-                                 dataset_treated[is.na(dataset_treated)] <- unknown_action$rep
-                               }
-                               return(dataset_treated)
-                             },
-                             removeInfinites = function(dataset, inf_action = list( act= "delete", rep_pos = 1, rep_neg = 0),...) {
-                               'Replace +Inf with and -Inf with desired values'
-                               dataset_treated       <- dataset
-                               inf_info              <- list( number = 0, technique = inf_action)
-                               if(inf_action$act == "delete") {
-                                 inf_action$rep_neg <- NA
-                                 inf_action$rep_pos <- NA
-                               }
-                               for (i in 1:ncol(dataset_treated)) {
-                                 a <- dataset_treated[,i]
-                                 if(is.numeric(a)) {
-                                   a[is.infinite(a) & a < 0] <- inf_action$rep_neg
-                                   a[is.infinite(a) & a > 0] <- inf_action$rep_neg
-                                   dataset_treated[,i]  <- a
-                                   inf_info$number <- inf_info$number + length(a[is.infinite(a) & a < 0] ) + length(a[is.infinite(a) & a > 0])
-                                 }
-                                 
-                               }
-                               info_$Infinites       <<- inf_info
-                               return(dataset_treated)
-                             },
-                             removeOutliers = function(dataset, thres_factor = 1.5, outlier_action = list(rep = 0), ...) {
-                               'Replaces outliers with desired value'
-                               dataset_treated <- dataset
-                               sum_below       <- 0
-                               sum_above       <- 0
-                               for(i in 1:length(dataset)) {
-                                 x                    <- dataset[,i]
-                                 qnt                  <- quantile(x, probs=c(.25, .75), na.rm = TRUE, ...)
-                                 H                    <- thres_factor * IQR(x, na.rm = TRUE) # iqr = upper_quantile - lower_quantile
-                                 y                    <- x
-                                 y[x < (qnt[1] - H)]  <- outlier_action$rep
-                                 y[x > (qnt[2] + H)]  <- outlier_action$rep
-                                 dataset_treated [,i] < -x
-                                 sum_below            <- sum_below + sum((x < (qnt[1] - H)))
-                                 sum_above            <- sum_above + sum((x > (qnt[1] - H)))
-                               }
-                               out_info       <- list( number_below = sum_below, number_above = sum_above,
-                                                       replacement = outlier_action$rep)
-                               info_$Outliers <<- out_info
-                               return(dataset_treated)
-                             },
-                             getInfo = function(...) {
-                               'Return information about removal of inappropriate values'
-                               return(info_)
-                             },
-                             initialize = function(...) {
-                               info_ <<- list()
-                               callSuper(...)
-                               .self
-                             }
+                             )
                            )
+
+InapRemover$methods(
+  #' Remove unknown entries
+  #' 
+  #' Deletes instances with unknown entries or replaces them with predefined value.  
+  #'
+  #' @name removeUnknown
+  #' @alias removeUnknown
+  #' 
+  #' @param dataset input dataset
+  #' @param unknown_action list that indicates action and replacement value
+  #' 
+  #' @return dataset with unknown values removed
+  removeUnknown = function(dataset, unknown_action = list( act = "replace", rep = 0), ...) {
+    'Treats unknown values according to the action (a list indicating whether na entries should lead to deletion of rows or replacement with desired value)'
+    if(unknown_action$act == "delete") {
+      dataset <- na.omit(dataset)
+    }
+    else if(unknown_action$act == "replace") {
+      dataset[is.na(dataset)] <- unknown_action$rep
+    }
+    # update info 
+    na_info         <- list( number = sum(is.na(dataset)), technique = unknown_action)
+    info_$NAs       <<- na_info
+    return(dataset)
+  },
+  #' Remove infinite values
+  #' 
+  #' Deletes instances with infinite values or replaces them with predefined value.  
+  #'
+  #' @name removeInfinites
+  #' @alias removeInfinites
+  #' 
+  #' @param dataset input dataset
+  #' @param inf_action list that indicates action and replacement value
+  #' 
+  #' @return dataset with infinite values removed
+  removeInfinites = function(dataset, inf_action = list( act= "delete", rep_pos = 1, rep_neg = 0),...) {
+    'Replace +Inf with and -Inf with desired values'
+    if(inf_action$act == "delete") {
+      inf_action$rep_neg <- NA
+      inf_action$rep_pos <- NA
+    }
+    inf_info              <- list( number = 0, technique = inf_action)
+    for (i in 1:ncol(dataset)) {
+      a <- dataset[,i]
+      if(is.numeric(a)) {
+        a[is.infinite(a) & a < 0] <- inf_action$rep_neg
+        a[is.infinite(a) & a > 0] <- inf_action$rep_neg
+        dataset[,i]  <- a
+        inf_info$number <- inf_info$number + length(a[is.infinite(a) & a < 0] ) + length(a[is.infinite(a) & a > 0])
+      }
+    }
+    # update info
+    info_$Infinites       <<- inf_info
+    return(dataset_treated)
+  },
+  #' Remove outliers
+  #' 
+  #' First determines outliers according to Tukey's test  and then removes them. 
+  #'
+  #' @name removeOutliers
+  #' @alias removeOutliers
+  #' 
+  #' @param dataset input dataset
+  #' @param thres_factor factor that multiplied with interquartile range gives outlier bounds
+  #' @param outlier_action list that contains replacement value
+  #' 
+  #' @return dataset with outliers removed
+  removeOutliers = function(dataset, thres_factor = 1.5, outlier_action = list(rep = 0), ...) {
+    'Replaces outliers with desired value'
+    dataset_treated <- dataset
+    sum_below       <- 0
+    sum_above       <- 0
+    for(i in 1:length(dataset)) {
+      x                    <- dataset[,i]
+      qnt                  <- quantile(x, probs=c(.25, .75), na.rm = TRUE, ...)
+      H                    <- thres_factor * IQR(x, na.rm = TRUE) # iqr = upper_quantile - lower_quantile
+      y                    <- x
+      y[x < (qnt[1] - H)]  <- outlier_action$rep
+      y[x > (qnt[2] + H)]  <- outlier_action$rep
+      dataset_treated [,i] < -x
+      sum_below            <- sum_below + sum((x < (qnt[1] - H)))
+      sum_above            <- sum_above + sum((x > (qnt[1] - H)))
+    }
+    # update info
+    out_info       <- list( number_below = sum_below, number_above = sum_above,
+                            replacement = outlier_action$rep)
+    info_$Outliers <<- out_info
+    return(dataset_treated)
+  },
+  #' Return information about InapRemover
+  #' 
+  #' Information includes removal of infinite, unknown and extreme values.
+  #' 
+  #' @name getInfo
+  #' @alias getInfo
+  #' 
+  #' 
+  #' @return list of information
+  getInfo = function(...) {
+    'Return information about removal of inappropriate values'
+    return(info_)
+  },
+  initialize = function(...) {
+    info_ <<- list()
+    callSuper(...)
+    .self
+  }
 )
